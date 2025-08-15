1807f9440e0096a695424da7f90514cde7264249
Dmitrii Oleinik <wedil@mail.ru>
2025-08-16 01:05:53 +0500

    add ReqresTests

--- src/test/java/ReqresTests.java
+++ src/test/java/ReqresTests.java
@@ -1,57 +1,47 @@
 
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
 public class ReqresTests extends TestBase {
-    static public int userID = 2;
-
-
-    @Test
-    void postLoginTest() {
-
-            String authData = "{\\\"email\\\": \\\"login@reqres.in\\\", \\\"password\\\": \\\"pass123\\\"}";
-
-            given()
-                   //.header("x-api-key",apiKey)
-                    .body(authData)
-                    .contentType("application/json")
-                    .log().uri()
-
-                    .when()
-                    .post("/login")
-
-                    .then()
-                    .log().status()
-                    .log().body()
-                    .statusCode(400)
-                    .body("error", notNullValue());
-        }
+    // static public int userID = 2;
 
     @Test
-    void testGetUserByID() {
-
+    void testErrorLogin() {
+        String authData = "{\\\"email\\\": \\\"login@reqres.in\\\", \\\"password\\\": \\\"pass123\\\"}";
         given()
-                //.header("x-api-key",apiKey)
-                .pathParam("id", userID)
+                .body(authData)
+                .contentType("application/json")
                 .log().uri()
+                .when()
+                .post("/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", notNullValue());
+    }
 
+    @Test
+    void testGetSingleUserSuccess() {
+        given(requestSpec)
+                .log().uri()
                 .when()
                 .get("/users/{id}")
-
                 .then()
                 .log().status()
                 .log().body()
                 .statusCode(200)
-                .body("data.id", equalTo(userID)) ;
+                .body("data.id", equalTo(userID));
     }
 
     @Test
-    void fetchesUsersTest() {
+    void testGetUsersSuccess() {
         given()
-               // .header("x-api-key",apiKey)
+                .header("x-api-key", apiKey)
                 .log().uri()
                 .when()
                 .get("/users")
@@ -61,43 +51,39 @@ public class ReqresTests extends TestBase {
     }
 
     @Test
-    public void deleteUserSuccessfully() {
-        int userId = 2;
-        given()
-                .header("x-api-key",apiKey)
+    public void testDeleteUserSuccessfully() {
+        given(requestSpec)
+                .header("x-api-key", apiKey)
                 .log().uri()
                 .when()
-                .delete("/users/" + userId)
+                .delete("/users/{id}")
                 .then()
                 .log().status()
                 .assertThat().statusCode(204);
     }
 
     @Test
-    public void deleteUserSuccessfully401() {
-        int userId = 2;
-        given()
-                .header("x-api-key",apiKey + 2)
+    public void testDeleteUserFailsWithForbiddenAccess() {
+        given(requestSpec)
+                .header("x-api-key", apiKey + 2)
                 .log().uri()
                 .when()
-                .delete("/users/" + userId)
+                .delete("/users/{id}")
                 .then()
                 .log().status()
                 .assertThat().statusCode(403);
     }
 
     @Test
-    public void updateUserWithPutRequest() {
-        int userId = 2;
-        Response response = given()
-                .header("x-api-key",apiKey)
+    public void testUpdateUserWithPutRequest() {
+        given(requestSpec)
+                .header("x-api-key", apiKey)
                 .log().uri()
                 .when()
-                .put("/users/" + userId);
-        response.then()
+                .put("/users/{id}")
+                .then()
                 .log().status()
                 .statusCode(200);
-
     }
-    }
+}
 
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -3,11 +3,20 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
+
+    protected static String apiKey = System.getProperty("API_KEY", "reqres-free-v1");
+
+    protected RequestSpecification requestSpec;
+    protected static int userID = 2;
+
+    public TestBase() {
+        this.requestSpec = RestAssured.given();
+        this.requestSpec.pathParam("id", userID);
+    }
+
     @BeforeAll
     static void setUp() {
         RestAssured.baseURI = "https://reqres.in";
         RestAssured.basePath = "/api";
     }
-    protected static String apiKey = System.getProperty("API_KEY", "reqres-free-v1");
-
 }
